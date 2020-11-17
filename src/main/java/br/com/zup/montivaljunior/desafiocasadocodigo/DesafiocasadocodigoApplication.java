570921639761e74fcfc9efa58a89ff3b5b074118
montivaljuniorzup <montival.junior@zup.com.br>
2020-11-17 14:55:23 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/zup/montivaljunior/desafiocasadocodigo/DesafiocasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.montivaljunior.desafiocasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocasadocodigoApplication.class, args);
+	}
+
+}
